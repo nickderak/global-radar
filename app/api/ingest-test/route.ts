5b2d96e35a807c95630f85b2a2cc5030b9147420
Nick Derakhshani <nickderakhshani@Nicks-MacBook-Pro.local>
2026-03-18 13:11:26 -0700

    Fix ingest-test dynamic route

--- app/api/ingest-test/route.ts
+++ app/api/ingest-test/route.ts
@@ -2,6 +2,8 @@ import { NextResponse } from "next/server";
 import { ingestSignals } from "../../lib/ingestSignals";
 import { logIngestionRun } from "../../lib/logIngestionRun";
 
+export const dynamic = "force-dynamic";
+
 export async function GET() {
   const testSignals = [
     {
