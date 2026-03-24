092941fef126de1e025a38461be3fcef3736df17
Nick Derakhshani <nickderakhshani@Nicks-MacBook-Pro.local>
2026-03-24 16:33:05 -0700

    Switch live registry from China to UN

--- app/lib/signalSources/index.ts
+++ app/lib/signalSources/index.ts
@@ -1,14 +1,14 @@
 import { usgsEarthquakeSource } from "./usgsEarthquakeSource";
 import { nwsAlertsSource } from "./nwsAlertsSource";
 import { gdacsAlertsSource } from "./gdacsAlertsSource";
-import { chinaGovSource } from "./chinaGovSource";
 import { ukFcdoSource } from "./ukFcdoSource";
+import { unSecurityCouncilSource } from "./unSecurityCouncilSource";
 import type { SignalSource } from "./types";
 
 export const registeredSignalSources: SignalSource[] = [
   usgsEarthquakeSource,
   nwsAlertsSource,
   gdacsAlertsSource,
-  chinaGovSource,
   ukFcdoSource,
+  unSecurityCouncilSource,
 ];
