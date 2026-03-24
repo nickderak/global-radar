753ff3880bd9721c2ce96918d519e6083fb5e350
Nick Derakhshani <nickderakhshani@Nicks-MacBook-Pro.local>
2026-03-24 11:22:09 -0700

    Disable dead sources and keep stable feed registry

--- app/lib/signalSources/index.ts
+++ app/lib/signalSources/index.ts
@@ -1,28 +1,14 @@
-import { mockWorldSource } from "./mockWorldSource";
 import { usgsEarthquakeSource } from "./usgsEarthquakeSource";
 import { nwsAlertsSource } from "./nwsAlertsSource";
 import { gdacsAlertsSource } from "./gdacsAlertsSource";
-import { indiaGovSource } from "./indiaGovSource";
-import { japanMofaSource } from "./japanMofaSource";
-import { israelGovSource } from "./israelGovSource";
-import { unSecurityCouncilSource } from "./unSecurityCouncilSource";
-import { pakistanGovSource } from "./pakistanGovSource";
 import { chinaGovSource } from "./chinaGovSource";
-import { mexicoGovSource } from "./mexicoGovSource";
 import { ukFcdoSource } from "./ukFcdoSource";
 import type { SignalSource } from "./types";
 
 export const registeredSignalSources: SignalSource[] = [
-  mockWorldSource,
   usgsEarthquakeSource,
   nwsAlertsSource,
   gdacsAlertsSource,
-  indiaGovSource,
-  japanMofaSource,
-  israelGovSource,
-  unSecurityCouncilSource,
-  pakistanGovSource,
   chinaGovSource,
-  mexicoGovSource,
   ukFcdoSource,
 ];
